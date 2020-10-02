dc5d660552e14dca6c2c72ad3ab0457d31e1b773
Дмитрий Брезгунов <dmitry.brezgunov@yandex.ru>
2020-10-02 18:42:01 +0300

    filtering formats

--- api/filters.py
+++ api/filters.py
@@ -7,9 +7,12 @@ class TasksFilter(filters.FilterSet):
     '''Фильтр для поиска задач с датой выполнения в заданом диапазоне'''
 
     date_from = filters.DateTimeFilter(
-        field_name="completion_date", lookup_expr='gte')
+        field_name="completion_date", lookup_expr='gte',
+        input_formats=['%d-%m-%Y %H:%M', '%d-%m-%Y'])
+
     date_to = filters.DateTimeFilter(
-        field_name="completion_date", lookup_expr='lte')
+        field_name="completion_date", lookup_expr='lte',
+        input_formats=['%d-%m-%Y %H:%M', '%d-%m-%Y'])
 
     class Meta:
         model = Task
